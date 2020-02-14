c3d0904f5d1ae429566198a5dc435862fd04cfce
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 02:47:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -5,11 +5,11 @@ JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
-
+CONST_NUM_1 = 1
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
-  CONST_NUM_1 = 1
+
   return [CONST_NUM_1]
 end
 
